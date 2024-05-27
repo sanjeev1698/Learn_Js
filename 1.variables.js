f4a2c43993c5822bcb3a3964cd5a0a96165c2fa3
SANJEEVRAM M <144574402+sanjeev1698@users.noreply.github.com>
2024-05-27 16:27:40 +0530

    commit

---
+++ 1.variables.js
@@ -0,0 +1,17 @@
+// Refer Documention in Mdn 
+
+//  Day 1 --> 3 types of  Variables : var,let,const.
+
+// var can redeclare and initilize the value
+var a = 1;
+console.log(a);
+
+var a = 2;
+console.log(a);
+
+// let --> can't redeclare and initilize the value
+let b = 2 ;
+console.log(b);
+// const --> constant value
+const c = 4;
+console.log(c);
