3058059cd01f7ad165ee77eaea69e6b79aa5052a
SANJEEVRAM M <144574402+sanjeev1698@users.noreply.github.com>
2024-05-27 16:28:13 +0530

    commit

---
+++ 2.datatype.js
@@ -0,0 +1,19 @@
+
+// Data Type (typeof used for find the data type)
+console.log(typeof("Hello World"));
+console.log(typeof(1));
+console.log(typeof(true));
+
+// in this case string added to the number value
+ x = "panda";
+ y = 1;
+console.log(x+y); 
+
+// it denotes the comparioson value
+x = "panda";
+y = 1;
+console.log(x === 1); 
+x = "panda";
+y = 1;
+console.log(x === "panda");
+
