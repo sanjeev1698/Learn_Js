7158a60c015d49942ca458ec1ac640e5d69d729e
SANJEEVRAM M <144574402+sanjeev1698@users.noreply.github.com>
2024-05-28 23:22:16 +0530

    commit

---
+++ 3.string.js
@@ -0,0 +1,20 @@
+
+// String Properties
+let t = "panda is an Animal";
+console.log(t.length);// it finds the lentgth of the string
+
+console.log(t.charAt(2)); // prints the index value of the character
+
+console.log(t.indexOf("a"));// find the index position
+
+console.log(t.lastIndexOf("a"));// find the last index
+
+console.log(t.toUpperCase());// convert to capital letters
+
+console.log(t.toLowerCase());//convert to small letter
+
+console.log(t.includes("panda"));// it check the containg letters,words boolean type
+
+console.log(t.slice(3,5));// form the sentence
+
+console.log(t.split(" "));// this used to spilt thye words
