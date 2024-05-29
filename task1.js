607c3028cc6b897585923c9b1ffad0cd5375b7cc
SANJEEVRAM M <144574402+sanjeev1698@users.noreply.github.com>
2024-05-29 10:45:21 +0530

    commit

--- task1.js
+++ task1.js
@@ -1,5 +1,4 @@
 console.log("panda".charAt(Math.random()*5));
 
 const a = "panda";
-
-console.log(a.charAt(Math.floor(Math.random()*a.length)));
+console.log(a.charAt(Math.floor(Math.random()*a.length))); 
